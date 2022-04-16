248d644e1982a821346158ebaf527aa2c0d68de0
alexalvarezg <alex2000alvarez@gmail.com>
2022-04-16 16:33:09 +0200

    añadido template y funcion para añadir seleccion (PERO NO FUNCIONA)

--- app/views.py
+++ app/views.py
@@ -757,9 +757,10 @@ def add_selection_form():
        seleccion_schema = SeleccionSchema(many=True)
        selecciones = seleccion_schema.dump(get_selecciones)
         # Estudiantes should have the object titles
-       return make_response(jsonify({"Selecciones": selecciones}))
+       output = db.engine.execute('SELECT E.id, E.nombre, E.apellidos, E.curso, E.grado, S.año, S.cuatri, S.vuelta, U.nombre FROM estudiantes E, seleccion S, aux_estudiante_seleccion A, universidad U WHERE E.id=A.id_estudiante AND S.id=A.id_seleccion AND U.id=S.confirmacion;').fetchall()
+       return render_template('Seleccion.html',result=output)
 
-    return render_template("formulario_seleccion.html")
+    return render_template("Nueva_Seleccion.html")
     
     
 
