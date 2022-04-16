e0a11fb0033eb1bc8b9daab292a298e238420277
alexalvarezg <alex2000alvarez@gmail.com>
2022-04-16 13:41:07 +0200

    creada vista para proceso de seleccion y actualizado index

--- app/views.py
+++ app/views.py
@@ -68,6 +68,13 @@ def select_asignaturasOD():
     return render_template('AsignaturasDestinoOrigen.html',result=output)
 
 
+@app.route("/seleccion")
+def select_selection():
+    output = db.engine.execute('SELECT E.id, E.nombre, E.apellidos, E.curso, E.grado, S.año, S.cuatri, S.vuelta, U.nombre FROM estudiantes E, seleccion S, aux_estudiante_seleccion A, universidad U WHERE E.id=A.id_estudiante AND S.id=A.id_seleccion AND U.id=S.confirmacion;').fetchall()
+    return render_template('Seleccion.html',result=output)
+
+
+
 '''
 TITULO
 '''
