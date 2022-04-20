1d381ae46d58dddd786d7699aff655ad8bd0ed86
alexalvarezg <alex2000alvarez@gmail.com>
2022-04-20 18:36:05 +0200

    añadidas carpetas

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,13 @@ app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://' + user + ':' + password
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint #from .main
+app.register_blueprint(main_blueprint)
 
 from app import views
 
