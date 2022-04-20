fad0beb2a57757192cb7036b8b21b9c9925b4f4b
alexalvarezg <alex2000alvarez@gmail.com>
2022-04-20 20:01:22 +0200

    cambiso en incorporacion de logo, layout e index

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask import redirect, jsonify, make_response, render_template, request
 from flask import render_template
 from sqlalchemy import text
 
-
+#para el login y registration
 from flask import Flask,render_template,request,redirect,session,flash,url_for
 from functools import wraps
 
@@ -25,8 +25,38 @@ def login():
         else:
             flash('Invalid Login. Try Again','danger')
     return render_template("login.html")
+#check if user logged in
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args,**kwargs):
+		if 'logged_in' in session:
+			return f(*args,**kwargs)
+		else:
+			flash('Unauthorized, Please Login','danger')
+			return redirect(url_for('login'))
+	return wrap
 
 
+
+@app.route('/reg',methods=['POST','GET'])
+def reg():
+    status=False
+    if request.method=='POST':
+        name=request.form["uname"]
+        email=request.form["email"]
+        pwd=request.form["upass"]
+        db.engine.execute("insert into users(nombre,password,email) values(%s,%s,%s)",(name,pwd,email))
+        db.session.commit()
+        flash('Registration Successfully. Login Here...','success')
+        return redirect('login')
+    return render_template("register.html",status=status)
+
+#logout
+@app.route("/logout")
+def logout():
+	session.clear()
+	flash('You are now logged out','success')
+	return redirect(url_for('login'))
 
 @app.route("/")
 def index_prueba():
